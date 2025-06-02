42dad9460febd3220aa4cd0242f8b26076c54422
Omer Parnes <you@example.com>
2025-06-02 22:22:47 +0300

    add constants class

--- Chess/include/Constants.h
+++ Chess/include/Constants.h
@@ -2,7 +2,16 @@
 // Created by omer on 5/27/2025.
 //
 
-#ifndef CHESS_CONSTVALUES_H
-#define CHESS_CONSTVALUES_H
+#ifndef CHESS_CONSTANTS_H
+#define CHESS_CONSTANTS_H
 
-#endif //CHESS_CONSTVALUES_H
+namespace Constants {
+    constexpr int PRIORITY_QUEUE_SIZE = 5; // Size of the priority queue for move evaluation
+    constexpr int CHECK_STATUS = 40; // Move that checks if the move is valid
+    constexpr int NUMBER_OF_MOVES_FOR_AUTO_GAME = 8; // Number of moves to simulate in automatic game mode
+    constexpr int AUTOMATIC_MODE = 1; // Automatic mode for the game
+    constexpr int MANUAL_MODE = 2; // Manual input mode for the game
+    constexpr int HIGH_SCORE_THRESHOLD = 15; // Threshold for high scoring moves in evaluation
+}
+
+#endif //CHESS_CONSTANTS_H
