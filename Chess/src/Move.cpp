9c06e7e4ce4c055405e7c87e0dbdf27e0c9581ee
Omer Parnes <you@example.com>
2025-06-02 22:25:23 +0300

    fixing the operator >>

--- Chess/src/Move.cpp
+++ Chess/src/Move.cpp
@@ -20,7 +20,7 @@ pair<int, int> Move::getEndPosition() { return endPosition; }
 bool Move::operator<(const Move& other) const { return score < other.score; }
 
 ostream& operator<<(ostream& os, const Move& move) {
-    os << static_cast<char>(move.startPosition.first + 'a') << move.startPosition.second + 1 << " "
+    os << static_cast<char>(move.startPosition.first + 'a') << move.startPosition.second + 1
        << static_cast<char>(move.endPosition.first + 'a') << move.endPosition.second + 1 << " with score: " << move.score;
     return os;
 }
